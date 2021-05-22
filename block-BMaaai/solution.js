01722134f16004e13f0cdd87c12471a97be40f02
Santosh Balchandran <santoshbalchandran@gmail.com>
2021-05-22 18:01:57 +0530

    Add code to solution.js

---
+++ block-BMaaai/solution.js
@@ -0,0 +1,12 @@
+mongoimport --db test --collection users --file /home/santosh/Desktop/cities.json --jsonArray
+
+mongoexport --db test --collection users --out /home/santosh/Desktop/exported.json --jsonArray
+
+mongoimport -d test -c stud --type csv --file "/home/santosh/Desktop/stud.csv" --headerline
+
+
+
+
+
+
+
